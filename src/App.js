b5f0085c239629e65328161db2250481bfb5007f
Jacob Trueb <jwtrueb@gmail.com>
2020-04-11 22:35:44 -0500

    Add show chart from S3

--- src/App.js
+++ src/App.js
@@ -10,38 +10,68 @@ const REGION = 'us-east-2';
 
 Amplify.Storage.configure({
   AWSS3: {
+    // devm TEST ENVIRONMENT
     bucket: 'covid-chi-105443-devm',
+    // prod ENVIRONMENT 
+    // bucket: 'covid-chi-1app-prod',
     region: REGION
   }
 });
-
-Amplify.Storage.list('')
-  .then(result => console.log(result))
-  .catch(err => console.log(err));
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      chartsByDate: [],
+      chartsByPatientId: [],
+      activeChart: logo,
+    };
+
+    Amplify.Storage.list('')
+      .then(result => {
+        let id = 0;
+        result.forEach(element => {
+          const chartRegex = /patient_id=([a-zA-Z0-9]+)\/gender=[mfo]\/yyyymmdd=(\d{8})\/.*.png/
+          const matches = element.key.match(chartRegex)
+          if(matches) {
+            console.log(element.key)
+            const patient_id = matches[1];
+            const date = matches[2];
+            const key = element.key;
+            this.state.chartsByPatientId.push({id: id, patient_id: patient_id, key: key});
+            this.state.chartsByDate.push({date: date, id: id, key: key});
+            id++;
+            this.setState({chartsByDate: this.state.chartsByDate, chartsByPatientId: this.state.chartsByPatientId}, () => {
+              console.log("A")
+              if(this.state.chartsByDate.length > 0) {
+                console.log("B")
+                console.log(this.state.chartsByDate[0].key)
+                Amplify.Storage.get(this.state.chartsByDate[0].key)
+                  .then((result) => {
+                    console.log("C")
+                    console.log(result);
+                    this.setState({activeChart: result})
+                  });
+              }
+            });
+          } 
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={this.state.activeChart} alt="logo" className="App-chart"/>
         </header>
+        <ol>{this.state.chartsByDate.map((v) => <li key={v.key}>{JSON.stringify(v)}</li>)}</ol>
+        <ol>{this.state.chartsByPatientId.map((v) => <li key={v.key}>{JSON.stringify(v)}</li>)}</ol>
+
+
       </div>
     );
   }
